5e8c79b9b3221ed62f8518df1a3c8da00d481091
dev7ch <silvan@dev7.ch>
2019-08-19 00:05:55 +0200

    revert install

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,19 @@ import { StyleBlack, StyleWhite, StyleMetal, Default, StyleFactory } from './sty
 export { WindowResizeEvent, fixPosition } from './components/window/script';
 export { StyleBlack, StyleWhite, StyleMetal, Default, StyleFactory };
 
-const options = { prefix: 'lom-window' };
-
-Vue.component(`${options.prefix}`, MyWindow);
-Vue.component(`${options.prefix}-default`, Default);
-Vue.component(`${options.prefix}-black`, StyleBlack);
-Vue.component(`${options.prefix}-white`, StyleWhite);
-Vue.component(`${options.prefix}-metal`, StyleMetal);
+const LomWindow = {
+    install(vue: typeof Vue, options = { prefix: 'lom-window' }) {
+        vue.component(`${options.prefix}`, MyWindow);
+        vue.component(`${options.prefix}-default`, Default);
+        vue.component(`${options.prefix}-black`, StyleBlack);
+        vue.component(`${options.prefix}-white`, StyleWhite);
+        vue.component(`${options.prefix}-metal`, StyleMetal);;
+    },
+};
 
 export { windows } from './windows';
 
 export const WindowType = (MyWindow as any) as typeof import('./components/window/script').WindowType;
 
+
+export default LomWindow;
