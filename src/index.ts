4aecfe4d346fdf47c65ce1a923bc01d586791e19
dev7ch <silvan@dev7.ch>
2019-08-18 23:47:12 +0200

    remove install vue mehod

--- src/index.ts
+++ src/index.ts
@@ -5,28 +5,17 @@ import { StyleBlack, StyleWhite, StyleMetal, Default, StyleFactory } from './sty
 export { WindowResizeEvent, fixPosition } from './components/window/script';
 export { StyleBlack, StyleWhite, StyleMetal, Default, StyleFactory };
 
-const LomWindow = {
-    install(vue: typeof Vue, options = { prefix: 'lom-window' }) {
-
-        vue.component(`${options.prefix}`, MyWindow);
-        vue.component(`${options.prefix}-default`, Default);
-        vue.component(`${options.prefix}-black`, StyleBlack);
-        vue.component(`${options.prefix}-white`, StyleWhite);
-        vue.component(`${options.prefix}-metal`, StyleMetal);
-        // We call Vue.mixin() here to inject functionality into all components.
-        vue.mixin({
-            // Anything added to a mixin will be injected into all components.
-            // In this case, the mounted() method runs when the component is added to the DOM.
-            mounted() {
-                console.log(this, 'Mounted!');
-            },
-        });
-    },
+const options = { prefix: 'lom-window' };
+const LomWindow = () => {
+    Vue.component(`${options.prefix}`, MyWindow);
+    Vue.component(`${options.prefix}-default`, Default);
+    Vue.component(`${options.prefix}-black`, StyleBlack);
+    Vue.component(`${options.prefix}-white`, StyleWhite);
+    Vue.component(`${options.prefix}-metal`, StyleMetal);
 };
 
 export { windows } from './windows';
 
 export const WindowType = (MyWindow as any) as typeof import('./components/window/script').WindowType;
-
 
 export default LomWindow;
